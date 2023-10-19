e6a5df9844d4833fa7e378a44fdaa1999a5f1fd4
GomezGra <gomezgra14@gmail.com>
2023-10-18 23:31:45 -0300

    cambiar nombre de ruta

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/primer_proyecto';
+    public string $baseURL = 'http://localhost/primer_proyecto_copia';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
